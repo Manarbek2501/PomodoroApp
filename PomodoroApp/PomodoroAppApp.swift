2740f93bd2524380931be0fa05bd3e3a4c05ad01
Manarbek.2501 <b.manarbek@icloud.com>
2023-04-22 20:34:01 +0600

    Initial Commit

---
+++ PomodoroApp/PomodoroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAppApp.swift
+//  PomodoroApp
+//
+//  Created by Manarbek Bibit on 22.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
